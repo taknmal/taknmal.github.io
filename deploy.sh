895e3ef98d1b8cae75b309970a995264f23bdb83
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-04 13:43:59 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd app-src
 npx webpack build
 cd ..
-cp app-src/dist/* ./
+cp -r app-src/dist/* ./
 cp app-src/src/*.js ./
 python3 add_js_files_to_sw_js.py
 git add . && git commit -am deploy && git push
