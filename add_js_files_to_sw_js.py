41bf1a76edca48f8795e2ccc29cc9f4a45d52b25
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-03-04 13:44:47 +0000

    deploy

--- add_js_files_to_sw_js.py
+++ add_js_files_to_sw_js.py
@@ -1,6 +1,6 @@
 import os
 
-files = ','.join([f"'/{file}'" for file in os.listdir() if file.endswith('.js')])
+files = ',\n'.join([f"'/{file}'" for file in os.listdir() if file.endswith('.js')])
 
 # Read in the file
 with open('sw.js', 'r') as file:
@@ -12,5 +12,5 @@ extra_files = f"'/{file}'"
 filedata = filedata.replace('const PRECACHE_URLS = [', 'const PRECACHE_URLS = [\n' + files)
 
 # Write the file out again
-with open('file.txt', 'w') as file:
+with open('sw.js', 'w') as file:
   file.write(filedata)
